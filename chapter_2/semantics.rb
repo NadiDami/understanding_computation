ae8134768d9ce0369ebdb2048c940c01ea20447c
Makis Otman <makisotman@gmail.com>
2014-07-28 20:27:16 +0100

    added reduce

--- chapter_2/semantics.rb
+++ chapter_2/semantics.rb
@@ -2,19 +2,13 @@ class Number < Struct.new(:value)
   def to_s
     value.to_s
   end
-  
-  def inspect
-    "#{self}"
-  end
-end
-
-class Multiply < Struct.new(:left, :right)
-  def to_s
-    "#{left} * #{right}"
-  end
 
   def inspect
     "#{self}"
+  end
+
+  def reducible?
+    false
   end
 end
 
@@ -26,7 +20,46 @@ class Add < Struct.new(:left, :right)
   def inspect
     "#{self}"
   end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Add.new(left.reduce, right)
+    elsif right.reducible?
+      Add.new(left, right.reduce)
+    else
+      Number.new(left.value + right.value)
+    end
+  end
 end
+
+class Multiply < Struct.new(:left, :right)
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "#{self}"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce
+    if left.reducible?
+      Multiply.new(left.reduce, right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce)
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+end
+
 
 result = Add.new(Multiply.new(Number.new(1), Number.new(10)),
                  Multiply.new(Number.new(2), Number.new(20)))
